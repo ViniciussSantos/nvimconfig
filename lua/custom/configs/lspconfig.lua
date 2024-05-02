821f7913cc794ab1b7c234cf72f80cce486688df
Vinicius Santos <vinicius@mixrank.com>
2024-05-02 20:55:14 -0300

    add a format hook for ruff_lsp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -135,7 +135,26 @@ lspconfig.pyright.setup {
 }
 
 lspconfig.ruff_lsp.setup {
-  on_attach = on_attach,
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = true
+    client.server_capabilities.documentRangeFormattingProvider = true
+
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
+        group = augroup,
+        buffer = bufnr,
+      }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
+
   capabilities = capabilities,
   cmd = { "ruff-lsp" },
   filetypes = { "python" },
