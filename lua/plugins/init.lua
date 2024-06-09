77e72525aa0ce28c1db6622c898a9e707ead7509
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-06-09 19:59:26 -0300

    redo nvchad nvimtree config to my tastes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -124,5 +124,24 @@ local plugins = {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "dmmulroy/tsc.nvim",
+    ft = { "typescript", "typescriptreact", "typescript.tsx" },
+    config = function()
+      require "configs.tsc"
+    end,
+    opts = {},
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = function()
+      return require "configs.nvimtree"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "nvimtree")
+      require("nvim-tree").setup(opts)
+    end,
+  },
 }
 return plugins
