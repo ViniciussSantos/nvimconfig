e83f6bd7a9659b6e9fcfbbc924d0f87d0ec5cd92
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-08-18 20:58:14 -0300

    add treesitter text objects

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -170,5 +170,30 @@ local plugins = {
       require("nvim-surround").setup {}
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    opts = function()
+      return require "configs.treesitter"
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    lazy = true,
+    opts = function()
+      return require "configs.treesitter-textobjects"
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
 return plugins
