260fd8a05eb8fdc8e885d284b77509676ef3d7de
Vinicius Santos <vinicius@mixrank.com>
2026-01-19 08:50:55 -0300

    add new autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -25,3 +25,26 @@ autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("wrap_spell", { clear = true }),
+  pattern = { "text", "plaintex", "typst", "gitcommit" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
